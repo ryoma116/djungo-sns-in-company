a06f113fcd923d250744acc18deab569d6defdc7
ryoma116 <uma19880116>
2020-09-22 06:20:05 +0900

    new login view

--- board_app/urls.py
+++ board_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import signup_func
+from .views import signup_func, login_func
 
 
 urlpatterns = [
-    path('signup/', signup_func),
+    path('signup/', signup_func, name='signup'),
+    path('login/', login_func, name='login'),
 ]
